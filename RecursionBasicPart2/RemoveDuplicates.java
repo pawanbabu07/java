ef07298824ccba4985de854a0157ab2db30c13db
pawanbabu07 <riturajpawan755@gmail.com>
2025-01-31 18:09:29 +0530

    update

--- RecursionBasicPart2/RemoveDuplicates.java
+++ RecursionBasicPart2/RemoveDuplicates.java
@@ -1,25 +1,27 @@
 package RecursionBasicPart2;
 //import java.lang.StringBuilder;
 public class RemoveDuplicates {
-    public static void removeCharecter(String str,int index, StringBuilder sb){
+    public static void removeCharecter(String str,int index,StringBuilder sb ,boolean ar[]){
         
-        boolean ar[]=new boolean[26];
+        
         if(index==str.length()){
             System.out.println(sb);
             return;
         } 
         char ch=str.charAt(index);
         if(ar[ch-'a']==true){
-            removeCharecter(str, index+1,sb);
+            removeCharecter(str, index+1,sb,ar);
         }else{
             ar[ch-'a']=true;
             sb=sb.append(ch);
-            removeCharecter(str, index+1, sb);
+            //System.out.println(sb);
+            removeCharecter(str, index+1, sb,ar);
         }
     }
        public static void main(String[] args) {
         StringBuilder sb=new StringBuilder("");
+        boolean ar[]=new boolean[26];
        String str="appanacollege";
-       removeCharecter(str, 0,sb);
+       removeCharecter(str, 0,sb,ar);
     }
 }
