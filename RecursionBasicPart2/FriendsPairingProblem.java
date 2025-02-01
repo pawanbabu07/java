37852cccd27c6d18fbfdb6d6e4b6950e0135d557
pawanbabu07 <riturajpawan755@gmail.com>
2025-02-01 23:52:33 +0530

    update

--- RecursionBasicPart2/FriendsPairingProblem.java
+++ RecursionBasicPart2/FriendsPairingProblem.java
@@ -5,8 +5,14 @@ package RecursionBasicPart2;
 
 public class FriendsPairingProblem {
 
-    
+    public static int pairing(int n){
+        if(n==1||n==2){
+            return n;
+        }
+        
+        return pairing(n-1)+(n-1)*pairing(n-2);
+    }
     public static void main(String args[]){
-
+    System.out.println(pairing(3));           
     }
 }
