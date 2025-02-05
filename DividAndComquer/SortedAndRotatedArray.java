b229afd035b81c581021df3c8ef367528ea45677
pawanbabu07 <riturajpawan755@gmail.com>
2025-02-06 00:38:50 +0530

    update

--- DividAndComquer/SortedAndRotatedArray.java
+++ DividAndComquer/SortedAndRotatedArray.java
@@ -1,6 +1,11 @@
 package DividAndComquer;
+/*
+ * NOTE:
+       Worst case occurs when pivot is slways
+       the smallest or the largest element;
+ */
 
-public class SortedAndRotatedArrayCode {
+public class SortedAndRotatedArray {
     public static int search(int arr[], int tar, int si, int ei) {
         if (si > ei) {
             return -1;
@@ -9,23 +14,23 @@ public class SortedAndRotatedArrayCode {
         if (arr[mid] == tar) {
             return mid;
         }
-        // Case: mid is on L1 (left sorted part)
+        // Case: mid is on L1 
         if (arr[si] <= arr[mid]) {
-            // Case a: target is in the left part
+            // Case a:left part
             if (arr[si] <= tar && tar <= arr[mid]) {
                 return search(arr, tar, si, mid - 1);
             } else {
-                // Case b: target is in the right part
+                // Case b:right part
                 return search(arr, tar, mid + 1, ei);
             }
         }
-        // Case: mid is on L2 (right sorted part)
+        // Case: mid is on L2 
         else {
-            // Case c: target is in the right part
+            // Case c:right part
             if (arr[mid] <= tar && tar <= arr[ei]) {
                 return search(arr, tar, mid + 1, ei);
             } else {
-                // Case d: target is in the left part
+                // Case d: left part
                 return search(arr, tar, si, mid - 1);
             }
         }
@@ -33,7 +38,7 @@ public class SortedAndRotatedArrayCode {
     public static void main(String[] args) {
         int arr[] = {4, 5, 6, 7, 0, 1, 2};
         int tar = 0;
-        int index = search(arr, tar, 0, arr.length - 1);
-        System.out.println(index);  // Output should be 5
+        int ind = search(arr, tar, 0, arr.length - 1);
+        System.out.println(ind);
     }
 }
