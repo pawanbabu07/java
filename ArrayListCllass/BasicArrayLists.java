df706c6e579680756f746c5fa3bef6e1e26caa1b
pawanbabu07 <riturajpawan755@gmail.com>
2025-02-22 15:00:29 +0530

    update

--- ArrayListCllass/BasicArrayLists.java
+++ ArrayListCllass/BasicArrayLists.java
@@ -11,6 +11,7 @@ public class BasicArrayLists {
         list.add(3);
         list.add(4);
         list.add(5);
+        list.add(6);
         System.out.println(list);
 
         //add element at index in arraylist
