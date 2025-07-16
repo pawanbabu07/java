43b2f22a0c09c5f6faf19610612f6a87b83bc285
pawanbabu07 <riturajpawan755@gmail.com>
2025-07-16 21:04:36 +0530

    tries

---
+++ TriesDataStructure/LongestWordWithAllPrefixes.java
@@ -0,0 +1,67 @@
+package TriesDataStructure;
+
+public class LongestWordWithAllPrefixes {
+    static class Node{
+        Node children[]= new Node[26];
+        boolean eow = false;
+        Node(){
+            for(int i=0;i<26; i++){
+                children[i]= null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    //Insert in tries
+    public static void insert(String word){  //O(L)
+        Node curr = root;
+        for(int level=0; level<word.length(); level++){
+            int idx = word.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                curr.children[idx]=new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow=true;
+    }
+
+    //Searching the words in tries
+    public static boolean search(String key){  //O(L)
+        Node curr = root;
+        for(int level=0; level<key.length(); level++){
+            int idx = key.charAt(level)-'a';
+            if(curr.children[idx]==null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow ==true;
+    }
+    static String ans = "";
+    public static void logestWord(Node root, StringBuilder temp){
+        if(root == null){
+            return;
+        }
+        for(int i=0;i<26;i++){
+            if(root.children[i] != null && root.children[i].eow == true){
+                char ch = (char)(i+'a');
+                temp.append(ch);
+                if(temp.length() > ans.length()){
+                    ans = temp.toString();
+                }
+                logestWord(root.children[i], temp);
+                temp.deleteCharAt(temp.length()-1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        String word[]={"a","banana","app","appl","ap","apply","apple"};
+        for(int i=0; i< word.length; i++){
+            insert(word[i]);
+        }
+        logestWord(root, new StringBuilder(""));
+        System.out.println(ans);
+        
+    }
+}
