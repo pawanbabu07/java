6d7d91b15bbec413ca5fb2732e88a58c6f3a7d1f
pawanbabu07 <riturajpawan755@gmail.com>
2025-08-22 00:18:23 +0530

    dp6mcm

---
+++ DynamicProgramming/dp6/MatrixChainMultiolicationMemoization.java
@@ -0,0 +1,33 @@
+package DynamicProgramming.dp6;
+
+import java.util.Arrays;
+
+public class MatrixChainMultiolicationMemoization {
+    public static int mcm(int[] arr, int i, int j, int[][] dp){
+        if(i==j){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+         int ans=Integer.MAX_VALUE;
+        for(int k=i;k<j;k++){
+            int cost1=mcm(arr, i, k, dp);
+            int cost2= mcm(arr, k+1, j, dp);
+            int cost3=arr[i-1]*arr[k]*arr[j];
+            int finalCost=cost1+cost2+cost3;
+            ans=Math.min(ans, finalCost);
+        }
+        return dp[i][j] = ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,3};
+        int n=arr.length;
+        int[][] dp=new int[n][n];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        System.out.println(mcm(arr, 1, n-1, dp));
+    }
+}
---
+++ DynamicProgramming/dp6/MatrixChainMultiolicationTabulation.java
@@ -0,0 +1,31 @@
+package DynamicProgramming.dp6;
+
+public class MatrixChainMultiolicationTabulation {
+    public static int mcm(int[] arr){
+        int n=arr.length;
+        int dp[][]=new int[n][n];
+        //initialization
+        for(int i=0;i<n;i++){
+            dp[i][i]=0;
+        }
+        //bottom-up
+        for(int len=2;len<=n-1;len++){
+            for(int i=1;i<=n-len;i++){
+                int j=i+len-1; //col
+                dp[i][j]=Integer.MAX_VALUE;
+                for(int k=i; k<=j-1;k++){
+                    int cost1= dp[i][k];
+                    int cost2=dp[k+1][j];
+                    int cost3 = arr[i-1]*arr[k]*arr[j];
+                    dp[i][j]=Math.min(dp[i][j], cost1+cost2+cost3);
+                }
+            }
+        }
+        return dp[1][n-1];
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,3};
+        System.out.println(mcm(arr));
+    }
+}
