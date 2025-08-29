1115d7620061f108229c00e5a52235cb116d622a
pawanbabu07 <riturajpawan755@gmail.com>
2025-08-29 20:03:07 +0530

    sigmentTree

--- SegmentTrees/MaxMinSegmentTreeQuearyUpdate.java
+++ SegmentTrees/MaxMinSegmentTreeQuearyUpdate.java
@@ -36,11 +36,38 @@ public class MaxMinSegmentTreeQuearyUpdate {
         return getMaxUtil(0, 0, n-1, qi, qj);
     }
 
+    public static void update(int arr[], int idx, int newVal){
+        arr[idx]=newVal;
+        int n=arr.length;
+        updateUtil(0, 0, n-1, idx, newVal);
+    }
+
+    public static void updateUtil(int i, int si, int sj, int idx, int newVal){   //O(logn)
+        if(idx<si || idx > sj){
+            return;
+        }
+        if(si==sj){
+            tree[i]=newVal;
+        }
+        if(si!=sj){
+            tree[i]=Math.max(tree[i], newVal);
+            int mid = (si+sj)/2;
+            updateUtil(2*i+1, si, mid, idx, newVal);
+            updateUtil(2*i+2, mid+1, sj, idx, newVal);
+        }
+    }
+
     public static void main(String[] args) {
         int arr[]={6,8,-1,2,17,1,3,2,4};
          int n=arr.length;
         init(n);
         build(arr, 0, 0, n-1);
+        for(int i:tree){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        System.out.println(getMax(arr, 2, 5));
+        update(arr, 2, 20);
         for(int i:tree){
             System.out.print(i+" ");
         }
